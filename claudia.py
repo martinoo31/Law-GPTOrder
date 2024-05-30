4148bf9a40fa48b294552c84bc627c209b847b00
KNGLJordan <giordani.francesco2002@gmail.com>
2024-05-30 18:07:31 +0200

    aggiornamento sistema di cartelle e stampa del res

--- claudia.py
+++ claudia.py
@@ -1,16 +1,27 @@
 import json
 import random
 import argparse
+import os
 
 # Codici ANSI per i colori
 BLUE = '\033[94m'
 GREEN = '\033[92m'
 RED = '\033[91m'
 ENDC = '\033[0m'
+GOLD = '\033[33m'
 
 def carica_domande(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+    domande = []
+    if os.path.isdir(file_path):
+        for filename in os.listdir(file_path):
+            if filename.endswith('.json'):
+                full_path = os.path.join(file_path, filename)
+                with open(full_path, 'r', encoding='utf-8') as file:
+                    domande.extend(json.load(file))
+    else:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            domande = json.load(file)
+    return domande
 
 def porre_domanda(domanda):
     print(f"\n{BLUE}{domanda['domanda']}{ENDC}")
@@ -42,16 +53,26 @@ def verifica_risposta(domanda, risposta):
 
 def main():
     parser = argparse.ArgumentParser(description="Applicazione di quiz a scelta multipla.")
-    parser.add_argument('file_domande', type=str, help="Il percorso del file JSON contenente le domande.")
+    parser.add_argument('file_domande', type=str, help="Il percorso del file JSON o della cartella contenente i file JSON con le domande.")
     args = parser.parse_args()
 
     domande = carica_domande(args.file_domande)
     random.shuffle(domande)
+
+    counter=0
+    counterTot=len(domande)
     
     for domanda in domande:
         risposta = porre_domanda(domanda)
-        verifica_risposta(domanda, risposta)
+        res=verifica_risposta(domanda, risposta)
+        if res:
+            counter+=1
+    
+    print(f"\n-----------------------------------------------")
+    print(f"\nHai risposto correttamente a {GOLD}{counter} domande su {counterTot}.{ENDC}")
+    print(f"Il tuo punteggio è del{GOLD} {counter/counterTot*100:.2f}%.{ENDC}")
+    print(f"Grazie per aver partecipato al quiz!")
+    print(f"\n-----------------------------------------------\n")
 
 if __name__ == "__main__":
     main()
-
